bca02e2e7c29c3c5b39f85329727ca824de2ec82
Alex Woods <alex.woods89@hotmail.co.uk>
2019-08-26 12:48:46 +0100

    customers added and working

--- ruby_project/db/vets.sql
+++ ruby_project/db/vets.sql
@@ -14,12 +14,12 @@ CREATE TABLE pets(
   type_of_animal VARCHAR(255),
   contact_details VARCHAR(255),
   treatment_notes VARCHAR(255),
-  vet_id INT8 REFERENCES vets(id) ON UPDATE CASCADE
+  vet_id INT8 REFERENCES vets(id)
 );
 
 CREATE TABLE customers(
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255),
   appointment TIMESTAMP,
-  pet_id INT8 REFERENCES pets(id) ON UPDATE CASCADE
+  pet_id INT8 REFERENCES pets(id)
 );
