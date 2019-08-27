0b03f3fabf8a86eb74b4fcb0d296d263dd2da4a1
Alex Woods <alex.woods89@hotmail.co.uk>
2019-08-27 09:33:16 +0100

    refactoring starting round 2

--- ruby_project/models/vet.rb
+++ ruby_project/models/vet.rb
@@ -21,6 +21,28 @@ class Vet
     @id = id.to_i
   end
 
+  def update()
+    sql = "UPDATE vets
+    SET
+    (
+      name
+    ) =
+    (
+      $1
+    )
+    WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM vets
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+
   def self.delete_all
     sql = 'DELETE FROM vets'
     SqlRunner.run(sql)
